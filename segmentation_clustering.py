c22e22ac2beba072338f1bbd3b8af5d9e5612c53
tokyokuma <comeonhokkaido@yahoo.co.jp>
2018-11-16 19:04:21 +0900

    complete clustering algorithm

--- segmentation_clustering.py
+++ segmentation_clustering.py
@@ -8,46 +8,34 @@ def main():
     cv2.imwrite(img_path, img)
     img = cv2.imread(img_path, 0)
 
+
+    #0.0083sec
     pole_threshold = Pole_Threshold(img)
-    start = time.time()
+    #0.0011sec
     pole_remove_noise = Open_Close(pole_threshold)
+    start = time.time()
+    #0.0040sec
+    label = cv2.connectedComponentsWithStats(pole_remove_noise)
     elapsed_time = time.time() - start
     print elapsed_time
 
-
-    '''
-    color_src01 = cv2.cvtColor(pole_threshold, cv2.COLOR_GRAY2BGR)
-    color_src02 = cv2.cvtColor(pole_threshold, cv2.COLOR_GRAY2BGR)
-    label = cv2.connectedComponentsWithStats(pole_threshold)
-    '''
-    color_src01 = cv2.cvtColor(pole_remove_noise, cv2.COLOR_GRAY2BGR)
-    color_src02 = cv2.cvtColor(pole_remove_noise, cv2.COLOR_GRAY2BGR)
-    label = cv2.connectedComponentsWithStats(pole_remove_noise)
-
     n = label[0] - 1
+
+    #remove backgrouond label
     data = np.delete(label[2], 0, 0)
     center = np.delete(label[3], 0, 0)
 
+    obstacle_coordination = np.zeros([n,2], dtype=np.float32)
+
     for i in range(n):
+        obstacle_coordination[i-1][0] = int(center[i][1])
+        obstacle_coordination[i-1][1] = int(center[i][0])
 
-        x0 = data[i][0]
-        y0 = data[i][1]
-        x1 = data[i][0] + data[i][2]
-        y1 = data[i][1] + data[i][3]
-        cv2.rectangle(color_src01, (x0, y0), (x1, y1), (0, 0, 255))
-        cv2.rectangle(color_src02, (x0, y0), (x1, y1), (0, 0, 255))
+    print obstacle_coordination
 
-        cv2.putText(color_src01, "ID: " +str(i + 1), (x1 - 20, y1 + 15), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255))
-        cv2.putText(color_src01, "S: " +str(data[i][4]), (x1 - 20, y1 + 30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255))
 
-        cv2.putText(color_src02, "X: " + str(int(center[i][0])), (x1 - 30, y1 + 15), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255))
-        cv2.putText(color_src02, "Y: " + str(int(center[i][1])), (x1 - 30, y1 + 30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 255))
-
-    cv2.imshow("pole_threshold", pole_threshold)
-    cv2.imshow("pole_remove_noise", pole_remove_noise)
-    cv2.imshow("color_src01", color_src01)
-    cv2.imshow("color_src02", color_src02)
-
+    #cv2.imshow("pole_threshold", pole_threshold)
+    #cv2.imshow("pole_remove_noise", pole_remove_noise)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
